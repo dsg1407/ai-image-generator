b74cd76fd32c66b55e58fa521b27288bb0c2f97d
Daniel S Goncalves <dsg1407@gmail.com>
2023-03-05 09:37:41 -0300

    Footer animation

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -8,12 +8,12 @@ export function Image() {
     <>
       <Dialog.Root>
         <Dialog.Trigger asChild className={styles.DialogTrigger}>
-          <img src="https://picsum.photos/256" />
+          <img src="https://picsum.photos/256/256?random=1" />
         </Dialog.Trigger>
         <Dialog.Portal>
           <Dialog.Overlay className={styles.DialogOverlay} />
           <Dialog.Content className={styles.DialogContent}>
-            <img src="https://picsum.photos/256" />
+            <img src="https://picsum.photos/256/256?random=1" />
             <Dialog.Close asChild className={styles.IconButton}>
               <X size={22} />
             </Dialog.Close>
